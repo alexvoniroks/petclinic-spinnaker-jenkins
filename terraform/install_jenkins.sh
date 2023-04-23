c1b0bd4d7200a97f743ab749f46872910f0a5cf6
alexvoniroks <120505253+alexvoniroks@users.noreply.github.com>
2023-04-23 20:48:54 +0300

    Update privileged actions

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -4,18 +4,18 @@
 sudo yum -y update
 
 echo "Install Java JDK 11"
-yum remove -y java
-yum install -y java-11-openjdk
+sudo yum remove -y java
+sudo yum install -y java-11-openjdk
 
 echo "Install Maven"
-yum install -y maven 
+sudo yum install -y maven 
 
 echo "Install git"
-yum install -y git
+sudo yum install -y git
 
 echo "Install Docker engine"
-yum update -y
-yum install docker -y
+sudo yum update -y
+sudo yum install docker -y
 #sudo usermod -a -G docker jenkins
 #sudo service docker start
 sudo chkconfig docker on
@@ -23,7 +23,7 @@ sudo chkconfig docker on
 echo "Install Jenkins"
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
-yum install -y jenkins
+sudo yum install -y jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
 sudo service docker start
